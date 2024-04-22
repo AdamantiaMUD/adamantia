4c98531618696d4baa027f1c24d6eaf920a4588d
Bill Parrott <bill@chimericdream.com>
2024-04-22 01:07:01 -0500

    chore: minor updates for website, soon to go away

---
+++ packages/adamantiamud.com/.eslintrc.cjs
@@ -0,0 +1,25 @@
+const base = require('../../.eslintrc.cjs');
+
+module.exports = {
+    ...base,
+    extends: [
+        ...base.extends,
+        '@chimericdream/jsx-a11y',
+        '@chimericdream/react',
+        '@chimericdream/react-hooks',
+    ],
+    root: true,
+    env: {
+        ...base.env,
+        browser: true,
+    },
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {},
+};
