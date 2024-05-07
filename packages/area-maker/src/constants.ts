26e3e8ed485f109b21d426e949a5ad96fa9631c0
Bill Parrott <bill@chimericdream.com>
2024-05-06 22:03:26 -0500

    chore: remove unused files

--- packages/area-maker/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const GRID_BUFFER = 450;
-export const GRID_SIZE = 30;
-export const DEBOUNCE_DELAY_FAST = 50;
-export const DEBOUNCE_DELAY_SLOW = 250;
--- packages/area-maker/src/utils/fns.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-/* eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any -- `any` is actually required here */
-export const cast = <T>(obj: any): T => obj as unknown as T;
